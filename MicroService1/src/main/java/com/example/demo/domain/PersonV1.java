e6ef469a7a28217a1787bc4f48d453089d05ceed
syl3335 <parthkumar.patel@kroger.com>
2020-02-08 13:43:32 -0500

    commit 2

---
+++ MicroService1/src/main/java/com/example/demo/domain/PersonV1.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain;
+
+public class PersonV1
+{
+    private String name;
+
+    public PersonV1(String name)
+    {
+        this.name = name;
+    }
+
+    public PersonV1()
+    {
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
+
